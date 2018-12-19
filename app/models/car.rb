7520916c0880d47d7de1cc99c43d5453003ce1e7
vincentbwalker <vincethewalker@gmail.com>
2018-12-19 12:41:51 +0000

    Added thumbnail image to index page

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,6 +5,7 @@ class Car < ApplicationRecord
     
     mount_uploader :picture, PictureUploader
     
+    
     def picture_size
        if picture_size > 5.megabytes
            errors.add(:picture, "should be less than 5MB")
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -13,6 +13,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
+  
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
@@ -30,9 +32,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+   version :thumb do
+     process resize_to_fit: [50, 50]
+   end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
