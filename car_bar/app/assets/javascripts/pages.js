82c35ca31eb9a09960f8c5b23bed92768a1b3f75
vincentbwalker <vincethewalker@gmail.com>
2019-03-22 15:49:21 +0000

    Amended Flash and Error banner CSS

---
+++ car_bar/app/assets/javascripts/pages.js
@@ -0,0 +1,9 @@
+/* global $, response */
+//Document ready
+$(document).on('turbolinks:load', function() {
+    $('.alert').delay(1000).fadeOut(4000);
+});
+
+$(document).on('turbolinks:load', function() {
+    $('.panel').delay(1000).fadeOut(4000);
+});
